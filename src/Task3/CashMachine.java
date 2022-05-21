7bb5cbdc5488fc21e5d329fa768e6db0be1406fa
Stonewall1 <impression97p@gmail.com>
2022-05-21 22:35:16 +0300

    ATM V2.0

--- src/Task3/CashMachine.java
+++ src/Task3/CashMachine.java
@@ -1,32 +1,84 @@
 package Task3;
 
 public class CashMachine {
+    int denomination10Amount;
     int denomination20Amount;
     int denomination50Amount;
     int denomination100Amount;
 
-    public CashMachine(int denomination20, int denomination50, int denomination100) {
+
+    public CashMachine(int denomination10 , int denomination20, int denomination50, int denomination100) {
+        this.denomination10Amount = denomination10;
         this.denomination20Amount = denomination20;
         this.denomination50Amount = denomination50;
         this.denomination100Amount = denomination100;
     }
 
-    public void cashMachineMoneyAddition(int d20 , int d50 , int d100){
-        this.denomination20Amount = this.denomination20Amount + d20;
-        this.denomination50Amount = this.denomination50Amount + d50;
-        this.denomination100Amount = this.denomination100Amount + d100;
+    public void cashMachineMoneyAddition(int denomination){
+        if(denomination == 10){
+            this.denomination10Amount = this.denomination10Amount + 1;
+        }
+        else if(denomination == 20){
+            this.denomination20Amount = this.denomination20Amount + 1;
+        }
+        else if(denomination == 50){
+            this.denomination50Amount = this.denomination50Amount + 1;
+        }
+        else if(denomination == 100){
+            this.denomination100Amount = this.denomination100Amount + 1;
+        }
+        else{
+            System.out.println("ATM is not working with that denomination");
+        }
     }
-    public void cashMachineMoneyRetrieving(int d20 , int d50 , int d100){
-        this.denomination20Amount = this.denomination20Amount - d20;
-        this.denomination50Amount = this.denomination50Amount - d50;
-        this.denomination100Amount = this.denomination100Amount - d100;
+    public void cashMachineMoneyRetrieving(int money){ //670
+       int sum = this.denomination10Amount*10 + this.denomination20Amount*20 + this.denomination50Amount*50 + this.denomination100Amount*100;
+       if(money <= sum){
+          while(money != 0){
+              int hundreds = money / 100; //6
+              if(hundreds <= this.denomination100Amount) {
+                  this.denomination100Amount = this.denomination100Amount - hundreds;
+                  money = money - hundreds*100;
+              }
+              else{
+                  money = money - this.denomination100Amount*100; //470
+                  this.denomination100Amount = 0;
+              }
+              int fiftys = money / 50; //9
+              if(fiftys <= this.denomination50Amount) {
+                  this.denomination50Amount = this.denomination50Amount - fiftys;
+                  money = money - fiftys*50;//20
+              }
+              else{
+                  money = money - this.denomination50Amount*50;
+                  this.denomination50Amount = 0;
+              }
+              int twentys = money / 20;//1
+              if(twentys <= this.denomination20Amount) {
+                  this.denomination20Amount = this.denomination20Amount - twentys;
+                  money = money - twentys*20; //0
+              }
+              else{
+                  money = money - this.denomination20Amount*20;
+                  this.denomination20Amount = 0;
+              }
+              int tens = money / 10;
+              if(tens <= this.denomination10Amount) {
+                  this.denomination10Amount = this.denomination10Amount - tens;
+                  money = money - tens*10; //0
+              }
+              else{
+                  money = money - this.denomination10Amount*10;
+                  this.denomination10Amount = 0;
+              }
+          }
+       }
+       else{
+           System.out.println("ATM dont have so much money");
+       }
     }
     public int currentATMMoney(){
-        return this.denomination20Amount*20 + this.denomination50Amount*50 + this.denomination100Amount*100;
+        return this.denomination10Amount*10 + this.denomination20Amount*20 + this.denomination50Amount*50 + this.denomination100Amount*100;
     }
-    public void beingRobbed(){
-        this.denomination20Amount = 0;
-        this.denomination50Amount = 0;
-        this.denomination100Amount = 0;
-    }
+
 }
--- src/Task3/Main.java
+++ src/Task3/Main.java
@@ -1,35 +1,19 @@
 package Task3;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        CashMachine Machine1 = new CashMachine(100 , 100 , 100);
+        CashMachine Machine1 = new CashMachine(10,10 , 10 , 10);
         int result1 = Machine1.currentATMMoney();
         System.out.println("Current CashMachine money : " + result1);
 
         //Adding money
-        Machine1.cashMachineMoneyAddition(100,50,10);
+        Machine1.cashMachineMoneyAddition(10);
         int result2 = Machine1.currentATMMoney();
         System.out.println("CashMachine has been filled : " + result2);
 
         //Retrieving money
-        Machine1.cashMachineMoneyRetrieving(12,45,88);
+        Machine1.cashMachineMoneyRetrieving(1790);
         int result3 = Machine1.currentATMMoney();
         System.out.println("CashMachine after money retrieving : " + result3);
-
-        //Robbery
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Do you want to rob the ATM? Enter YES or NO");
-        String decision = scanner.nextLine();
-        if(decision.equalsIgnoreCase("YES")){
-            Machine1.beingRobbed();
-            int result4 = Machine1.currentATMMoney();
-            System.out.println("CashMachine money after being robbed : " + result4);
-        }
-        else{
-            int result5 = Machine1.currentATMMoney();
-            System.out.println("CashMachine money left : " + result5);
-        }
     }
 }
