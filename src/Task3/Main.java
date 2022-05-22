89c2a0f262fb1e5824f8652e67a67247372e815f
Stonewall1 <impression97p@gmail.com>
2022-05-22 19:17:06 +0300

    ATM V3.0

--- src/Task3/Main.java
+++ src/Task3/Main.java
@@ -2,7 +2,7 @@ package Task3;
 
 public class Main {
     public static void main(String[] args) {
-        CashMachine Machine1 = new CashMachine(10,10 , 10 , 10);
+        CashMachine Machine1 = new CashMachine(100,100 , 100 , 100);
         int result1 = Machine1.currentATMMoney();
         System.out.println("Current CashMachine money : " + result1);
 
@@ -11,9 +11,32 @@ public class Main {
         int result2 = Machine1.currentATMMoney();
         System.out.println("CashMachine has been filled : " + result2);
 
-        //Retrieving money
-        Machine1.cashMachineMoneyRetrieving(1790);
+        System.out.println("Available denominations");
+        System.out.println(Machine1.denomination10Amount + " tens");
+        System.out.println(Machine1.denomination20Amount + " twentys");
+        System.out.println(Machine1.denomination50Amount + " fiftys");
+        System.out.println(Machine1.denomination100Amount + " hundreds");
+
+        //Standard money retrieving
+        Machine1.cashMachineMoneyRetrieving(330);
         int result3 = Machine1.currentATMMoney();
         System.out.println("CashMachine after money retrieving : " + result3);
+
+        System.out.println("Available denominations");
+        System.out.println(Machine1.denomination10Amount + " tens");
+        System.out.println(Machine1.denomination20Amount + " twentys");
+        System.out.println(Machine1.denomination50Amount + " fiftys");
+        System.out.println(Machine1.denomination100Amount + " hundreds");
+
+        //Advanced money retrieving
+        Machine1.advancedCashMachineMoneyRetrieving(390);
+        int result4 = Machine1.currentATMMoney();
+        System.out.println("CashMachine after advanced money retrieving : " + result4);
+
+        System.out.println("Available denominations");
+        System.out.println(Machine1.denomination10Amount + " tens");
+        System.out.println(Machine1.denomination20Amount + " twentys");
+        System.out.println(Machine1.denomination50Amount + " fiftys");
+        System.out.println(Machine1.denomination100Amount + " hundreds");
     }
 }
